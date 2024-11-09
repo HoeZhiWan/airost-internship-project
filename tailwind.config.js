d69458cc75a6b005b9ebf0eb1452eb6aa54889ea
Alfred Chin <alfredchin2005@gmail.com>
2024-11-09 23:19:40 +0800

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        shade: {
+          100: "#ECDFCC",
+          200: "#8B8B8B",
+          300: "#3F3F3F",
+          400: "#2F2F2F",
+          500: "#202020",
+          600: "#101010",
+        },
+        primary: {
+          tint: {
+            300: "#44C588",
+            400: "#08BD7A"
+          }
+        },
+        text: "#ffffff",
+      }
+    },
   },
   plugins: [],
 }
