6fef4e0a8f669013c91319bb240dd956a8777beb
Alfred Chin <alfredchin2005@gmail.com>
2024-11-10 18:24:29 +0800

    create reset route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage/RegisterPage"
 import LoginPage from "./pages/LoginPage/LoginPage"
 import ForgetPasswordPage from "./pages/ForgetPasswordPage/ForgetPasswordPage"
+import ResetPasswordPage from "./pages/ForgetPasswordPage/ResetPasswordPage"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route element={<RegisterPage />} path="/register" />
           <Route element={<LoginPage />} path="/login" />
           <Route element={<ForgetPasswordPage />} path="/forgetPassword" />
+          <Route element={<ResetPasswordPage />} path="/reset" />
         </Routes>
       </BrowserRouter>
     </div>
