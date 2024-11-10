f0c4ad225f82d010b0c76128091f4458201b053c
Alfred Chin <alfredchin2005@gmail.com>
2024-11-10 15:43:06 +0800

    create forgetPassword route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import RegisterPage from "./pages/RegisterPage/RegisterPage"
 import LoginPage from "./pages/LoginPage/LoginPage"
+import ForgetPasswordPage from "./pages/ForgetPasswordPage/ForgetPasswordPage"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route element={<RegisterPage />} path="/register" />
           <Route element={<LoginPage />} path="/login" />
+          <Route element={<ForgetPasswordPage />} path="/forgetPassword" />
         </Routes>
       </BrowserRouter>
     </div>
