8c2bce77e69652f692f9b07222832f9540aaf5a5
Alfred Chin <alfredchin2005@gmail.com>
2024-11-24 19:09:36 +0800

    Create navbar on main page

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,21 @@ import ResetPasswordPage from "./pages/ForgetPasswordPage/ResetPasswordPage"
 import ConfirmationPage from "./pages/RegisterPage/ConfirmationPage"
 import SetupPage from "./pages/RegisterPage/SetupPage"
 import VerifyPage from "./pages/RegisterPage/VerifyPage"
+import MainPage from "./pages/MainPage/MainPage"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route element={<RegisterPage />} path="/register" />
-          <Route element={<LoginPage />} path="/login" />
-          <Route element={<ForgetPasswordPage />} path="/forgetPassword" />
-          <Route element={<ResetPasswordPage />} path="/reset" />
-          <Route element={<ConfirmationPage />} path="/confirm" />
-          <Route element={<SetupPage />} path="/setup" />
-          <Route element={<VerifyPage />} path="/verify" />
+            <Route index element={<MainPage />} />
+            <Route element={<RegisterPage />} path="/register" />
+            <Route element={<LoginPage />} path="/login" />
+            <Route element={<ForgetPasswordPage />} path="/forgetPassword" />
+            <Route element={<ResetPasswordPage />} path="/reset" />
+            <Route element={<ConfirmationPage />} path="/confirm" />
+            <Route element={<SetupPage />} path="/setup" />
+            <Route element={<VerifyPage />} path="/verify" />
         </Routes>
       </BrowserRouter>
     </div>
