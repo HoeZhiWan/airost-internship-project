cd1fddda14c087e98168193d3f573e8af07d287f
Alfred Chin <alfredchin2005@gmail.com>
2024-11-26 22:41:25 +0800

    Add an input message box

--- src/components/Chat/ChatTab.jsx
+++ src/components/Chat/ChatTab.jsx
@@ -1,7 +1,9 @@
 function ChatTab() {
   return (
-    <div>
-      
+    <div className="flex justify-center items-end w-full m-4">
+      <form action="" className="w-full">
+            <input type="text" className="border-0 mt-[10px] w-full px-[12px] py-[8px] bg-shade-300 placeholder-text placeholder:italic rounded-[5px] focus:ring-0" placeholder="Type a message" />
+      </form>
     </div>
   )
 }
