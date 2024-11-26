181741f293fc41fc5c611b3dbd4a3c56a7f45092
Alfred Chin <alfredchin2005@gmail.com>
2024-11-26 23:49:58 +0800

    add ChatMessageComponent

--- src/components/Chat/ChatMessage.jsx
+++ src/components/Chat/ChatMessage.jsx
@@ -1,7 +1,20 @@
 function ChatMessage() {
   return (
-    <div>
-      
+    <div className="flex">
+        {/* profile picture */}
+        <img src="src\assets\Default_pfp.svg" alt="" className="w-12 h-12" />
+        <div className="ms-[10px] ">
+            <div className="font-bold flex items-baseline">
+                {/* Name */}
+                <div className="">Alfred</div>
+                {/* TImestamp */}
+                <div className="ms-2 font-thin text-[12px]">Today at 10:03 PM</div>
+            </div>
+            {/* chat messages */}
+            <div className="">
+                Text
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/Chat/ChatTab.jsx
+++ src/components/Chat/ChatTab.jsx
@@ -1,9 +1,13 @@
+import ChatMessage from "./ChatMessage"
+
 function ChatTab() {
   return (
-    <div className="flex justify-center items-end w-full m-4">
-      <form action="" className="w-full">
-            <input type="text" className="border-0 mt-[10px] w-full px-[12px] py-[8px] bg-shade-300 placeholder-text placeholder:italic rounded-[5px] focus:ring-0" placeholder="Type a message" />
-      </form>
+    <div className="flex flex-col gap-4 justify-end w-full m-4">
+        <ChatMessage />
+        <ChatMessage />
+        <form action="" className="w-full">
+            <input type="text" className="sticky border-0 w-full px-[12px] py-[8px] bg-shade-300 placeholder-text placeholder:italic rounded-[5px] focus:ring-0" placeholder="Type a message" />
+        </form>
     </div>
   )
 }
